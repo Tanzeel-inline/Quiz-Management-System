0de609591b0315940cd360dbd5577ade689aa17f
Tanzeel-inline <70278200+Tanzeel-inline@users.noreply.github.com>
2021-08-02 18:04:13 +0500

    asdasd

--- controller/appController.js
+++ controller/appController.js
@@ -37,7 +37,8 @@ exports.teacher_get_signup = (req,res)=>{
     res.sendFile(path.join(__dirname,'../views','teacher_signup.html')); 
 };
 
-exports.teacher_post_signup = (req,res)=>{
+exports.teacher_post_signup = async (req,res)=>{
+    //Validate if data is correct
     const schema = Joi.object().keys({
         email: Joi.string().trim().email().required() ,
         username: Joi.string().trim().max(60).required() ,
@@ -48,18 +49,33 @@ exports.teacher_post_signup = (req,res)=>{
     schema.validate(req.body, (err,result)=>{
         if ( err )
         {
-            res.send({success: false})
+            res.send({success: false});
+            return;
         }
-        else
-        {
-            res.send({success:true});
-        }
-        
     });
-    //Validate if data is correct
     //Check if email already exist 
+    const{email,username,password,contact,address} = req.body;
+    
+    const teacher = await TeacherModel.findOne({email});
     //Send failure message if exists
+    if ( teacher )
+    {
+        res.send({success: false});
+        return;
+    }
     //else (add the data to the database) and send success message
+    const pwHash = await bcrypt.hash(password, 12);
+
+    teacher = new TeacherModel({
+        email,
+        username,
+        password: pwHash,
+        contact,
+        address
+    });
+
+    await teacher.save();
+    
     console.log(req.body);
     res.send({success:true});
 };
